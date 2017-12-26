2bd1b43bc8ac75bc6e93a5ade50ebc1d96b846d4
Alexey.Ponimash <Alexey.Ponimash@gmail.com>
2017-12-26 10:58:57 +0300

    Changed generic config

--- homeassistant/components/device_tracker/generic.py
+++ homeassistant/components/device_tracker/generic.py
@@ -15,27 +15,20 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_URL, CONF_PASSWORD, CONF_USERNAME
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_URL): cv.string,
     vol.Optional(CONF_USERNAME): cv.string,
     vol.Optional(CONF_PASSWORD): cv.string
 })
 
 
-class InvalidLuciTokenError(HomeAssistantError):
-    """When an invalid token is detected."""
-
-    pass
-
-
 def get_scanner(hass, config):
     """Validate the configuration and return a Luci scanner."""
     scanner = GenericDeviceScanner(config[DOMAIN])
-
     return scanner if scanner.success_init else None
 
 
@@ -44,15 +37,11 @@ class GenericDeviceScanner(DeviceScanner):
 
     def __init__(self, config):
         """Initialize the scanner."""
-        self.host = config[CONF_HOST]
+        self.url = config[CONF_URL]
         #self.username = config[CONF_USERNAME]
         #self.password = config[CONF_PASSWORD]
-
-        #self.parse_api_pattern = re.compile(r"(?P<param>\w*) = (?P<value>.*);")
-
         self.last_results = {}
-        #self.refresh_token()
-        self.success_init = True#self.token is not None
+        self.success_init = True
 
 
     def scan_devices(self):
@@ -65,15 +54,13 @@ class GenericDeviceScanner(DeviceScanner):
 
     def _update_info(self):
         """Ensure the information from the Luci router is up to date.
-
         Returns boolean if scanning successful.
         """
         if not self.success_init:
             return False
-        url = 'http://{}/cgi-bin/assoclist.cgi'.format(self.host)
 
         try:
-            result = requests.get(url, timeout=5 ).json()
+            result = requests.get(self.url, timeout=5 ).json()
         except Exception:
             _LOGGER.exception("Error while retrieve STA list")
             return False
